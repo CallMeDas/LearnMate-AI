cc9658bb50ac5f825f7c65b1a81a55ca3a82cabe
callmedas <deepakkdas77@gmail.com>
2026-04-24 12:29:58 +0545

    README.md added

--- create_db.py
+++ create_db.py
@@ -1,7 +1,7 @@
 from langchain_mistralai import ChatMistralAI, MistralAIEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -20,11 +20,11 @@ splitter = RecursiveCharacterTextSplitter(
 chunks  = splitter.split_documents(docs)
 
 #Embeddigns Model
-embeddings_model =  MistralAIEmbeddings()
+embeddings_model =  MistralAIEmbeddings(model="mistral-embed")
 
 #Store in Chroma DB. 
 vectorstore = Chroma.from_documents(
-    documents=docs,
+    documents=chunks,
     embedding=embeddings_model,
     persist_directory='chroma_db'
 )
